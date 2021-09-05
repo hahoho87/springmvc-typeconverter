9e4c2eddce36216ad8f27ea07b48fd0eeabe3d03
hahoho87 <hahoho87@gmail.com>
2021-09-06 07:04:56 +0900

    Initial commit

---
+++ src/main/java/com/hahoho87/typeconverter/SpringmvcTypeconverterApplication.java
@@ -0,0 +1,13 @@
+package com.hahoho87.typeconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcTypeconverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcTypeconverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hahoho87/typeconverter/SpringmvcTypeconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hahoho87.typeconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcTypeconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
